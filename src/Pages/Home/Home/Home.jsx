9c8a8f9e4b699010d81e9dc1dbcce225951dab73
BD-Arfat <arfat1islam5162@gmail.com>
2025-01-06 23:22:19 +0600

    add oure_menu section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../Banner/Banner";
 import Order_online from "../Order_online/Order_online";
 import BistroBoss from "../BistroBoss/BistroBoss";
 import From_our_menu from "../From_our_menu/From_our_menu";
+import Our_menus from "../Our_menus/Our_menus";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Order_online></Order_online>
       <BistroBoss></BistroBoss>
       <From_our_menu></From_our_menu>
+      <Our_menus></Our_menus>
     </div>
   );
 };
