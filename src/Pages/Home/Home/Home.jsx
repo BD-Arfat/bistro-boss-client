cdf4ce07985e570e9c7859fe5ca7ac43f959717c
BD-Arfat <arfat1islam5162@gmail.com>
2025-01-07 22:41:31 +0600

    complite home page

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,6 +4,8 @@ import Order_online from "../Order_online/Order_online";
 import BistroBoss from "../BistroBoss/BistroBoss";
 import From_our_menu from "../From_our_menu/From_our_menu";
 import Our_menus from "../Our_menus/Our_menus";
+import Producats_cart from "../Product_carts/Producats_cart";
+import TESTIMONIALS from "../TESTIMONIALS/TESTIMONIALS";
 
 const Home = () => {
   return (
@@ -12,7 +14,9 @@ const Home = () => {
       <Order_online></Order_online>
       <BistroBoss></BistroBoss>
       <From_our_menu></From_our_menu>
+      <Producats_cart></Producats_cart>
       <Our_menus></Our_menus>
+      <TESTIMONIALS></TESTIMONIALS>
     </div>
   );
 };
---
+++ src/Pages/Home/Product_carts/Product.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Product = ({ product }) => {
+  const { image } = product;
+  return (
+    <div className=" bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+      <img
+        src={image} // এখানে আপনার ইমেজ URL যোগ করুন
+        alt="Caesar Salad"
+        className="w-full h-48 rounded-t-lg object-cover"
+      />
+      <div className="p-4">
+        <h3 className="text-lg font-bold text-gray-800">Caesar Salad</h3>
+        <p className="text-sm text-gray-600 mt-1">
+          Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.
+        </p>
+        <button className="mt-4 w-full px-4 py-2 text-white bg-yellow-500 hover:bg-yellow-600 rounded-lg focus:outline-none">
+          ADD TO CART
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
